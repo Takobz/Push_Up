87a55d0039700748a68d46cc1ff9d4e7f44467a3
Khutso Kobela <47497650+Takobz@users.noreply.github.com>
2022-04-14 21:50:37 +0200

    Update median.py

--- median.py
+++ median.py
@@ -1,10 +1,17 @@
 numbers = []
 number = 0
+median = 0.0
 n = int(input())
 
 for i in range(0, n):
     number = int(input())
     numbers.append(number)
+
+numbers = sorted(numbers)
+half = (n//2)
+if (n % 2) == 0:
+    median = (numbers[half] + numbers[half - 1])/2
+else:
+    median = numbers[half]
     
-median = sum(numbers)/2
 print(median)
